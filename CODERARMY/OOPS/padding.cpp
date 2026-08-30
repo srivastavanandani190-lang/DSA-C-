4b894090fd1381a5d5e7facebebaf8c0fd1e9952
Nandani Srivastava <srivastavanandani190@gmail.com>
2026-08-30 09:56:33 +0530

    padding

---
+++ CODERARMY/OOPS/padding.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+class a{
+    int i;
+    char c;
+    char k;//12
+    int j;
+    //char d;//16
+    
+};
+int main(){
+    a obj1l,obj2;
+    cout<<sizeof(obj1l)<<"\n";
+    cout<<sizeof(obj2)<<"\n";
+    return 0;
+}
